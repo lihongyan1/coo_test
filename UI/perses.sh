2bd94b8d61b3283641e085f6e55b2344ff8a3026
Hongyan Li <hongyli@redhat.com>
2025-12-25 14:35:37 +0800

    Refactor uiplugin

--- UI/perses.sh
+++
@@ -1,6 +0,0 @@
-oc create ns perses-dev
-oc apply -f https://raw.githubusercontent.com/etmurasaki/dotfiles/refs/heads/main/scripts/rbac/dashboards/openshift-cluster-sample-dashboard.yaml
-oc apply -f https://raw.githubusercontent.com/etmurasaki/dotfiles/refs/heads/main/scripts/rbac/dashboards/perses-dashboard-sample.yaml
-oc apply -f https://raw.githubusercontent.com/etmurasaki/dotfiles/refs/heads/main/scripts/rbac/dashboards/prometheus-overview-variables.yaml
-oc apply -f https://raw.githubusercontent.com/etmurasaki/dotfiles/refs/heads/main/scripts/rbac/dashboards/thanos-compact-overview-1var.yaml
-oc apply -f https://raw.githubusercontent.com/etmurasaki/dotfiles/refs/heads/main/scripts/rbac/dashboards/thanos-querier-datasource.yaml
